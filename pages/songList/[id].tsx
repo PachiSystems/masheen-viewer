03eefd895b6513f092d41f436d5641aca18e374f
Brian Milton <brianwilliam.milton@bourne-leisure.co.uk>
2021-04-09 17:57:35 +0100

    fix(status): great full combo changed to full combo

--- pages/songList/[id].tsx
+++ pages/songList/[id].tsx
@@ -173,13 +173,11 @@ const SongList: FunctionComponent = () => {
             case 'FullComboW2':
                 return <span className={classes.perfect}>Perfect Full Combo</span>;
             case 'FullComboW3':
-                return <span className={classes.great}>Great Full Combo</span>;
-            case 'FullComboW4':
-                return <span className={classes.good}>Full Combo</span>;
+                return <span className={classes.great}>Full Combo</span>;
             case 'SingleDigitW3':
-                return <><span className={classes.great}>Great Full Combo</span><br/><em>Single Digit Greats!</em></>
+                return <><span className={classes.great}>Full Combo</span><br/><em>Single Digit Greats!</em></>
             case 'OneW3':
-                return <><span className={classes.great}>Great Full Combo</span><br/><em>Only One Great!</em></>
+                return <><span className={classes.great}>Full Combo</span><br/><em>Only One Great!</em></>
             default:
                 return null
         }
